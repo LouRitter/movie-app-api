7aeca8b82f4b4ff014a034835eebd145d98e577c
louritter <louritter1@gmail.com>
2022-03-21 23:21:23 -0400

    dotenv

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,15 +1,15 @@
 class MovieService < ApplicationService
-    attr_reader :movie
-
     require 'faraday/net_http'
     Faraday.default_adapter = :net_http
+
+    attr_reader :movie
 
     def initialize(movie)
       @movie = movie
     end
   
     def search
-        @api_key = "6acbb971452c91042d1e6a3b6d068850" #ENV['MOVIEDB_API_KEY']
+        @api_key = ENV['MOVIEDB_API_KEY']
         response = Faraday.get("https://api.themoviedb.org/3/search/multi?api_key=#{@api_key}&query=#{@movie}")
         return response.body
     end
